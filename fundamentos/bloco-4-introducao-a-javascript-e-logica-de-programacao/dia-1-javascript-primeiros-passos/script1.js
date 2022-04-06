c4ce3ac810f8752e080ed6fdcd8dc63d6a63c8b7
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-06 18:57:13 -0300

    Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
@@ -1,11 +1,31 @@
-const a = 60;
-const b = 61;
-const c = 60;
-soma = a + b + c;
+let peca = "Rainha";
+peca = peca.toLowerCase();
 
-if (soma === 180) {
-    console.log(true);
-} else {
-    console.log(false);
-    console.log("Angulos inválidos, a soma dos angulos deve ser 180!");
+switch (peca) {
+    case "peão":
+        console.log("Peão --> Uma casa à frente, captura diagonal.");
+        break;
+
+    case "rei":
+        console.log("Rei --> Move uma casa para qualquer direção e não pode encostar em outro rei.");
+        break;
+    
+    case "bispo":
+        console.log("Bispo --> Move na diagonal.");
+        break;
+    
+    case "rainha":
+        console.log("Rainha --> Move para qualquer direção.");
+        break;
+
+    case "cavalo":
+        console.log("Cavalo --> Move  3 casas em L.");
+        break;
+    
+    case "torre":
+        console.log("Torre --> Move na horizontal e vertical.");
+        break;
+
+    default:
+        console.log("Peça inválida!")
 }
