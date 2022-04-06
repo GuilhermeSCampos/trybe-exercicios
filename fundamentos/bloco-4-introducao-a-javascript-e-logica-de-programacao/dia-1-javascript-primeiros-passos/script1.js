09aa9584e80ab538c5b3bf71121162006f60697f
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-06 19:58:44 -0300

    Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
@@ -1,9 +1,29 @@
-const a = 15;
-const b = 35;
-let lucro = b - a*1.2;
+let sal = 3000;
+let ir;
 
-if (a > 0 && b > 0) {
-    console.log("O lucro de mil vendas desse produto será de:",lucro*1000);
+if (sal > 5189.82) {
+    sal = sal - 570,88;
+} else if (sal >= 2594.93 && sal <= 5189.82) {
+    sal = sal*0.89;
+} else if (sal >= 1556.95 && sal <= 2594.92) {
+    sal = sal*0.91;
 } else {
-    console.log("Valor Inválido!")
+    sal = sal*0.92;
 }
+console.log(sal);
+
+if (sal > 4664.68) {
+    ir = sal*0.275 - 869.36;
+    sal = sal - ir;
+} else if (sal >= 3751.06 && sal <= 4664.68) {
+    ir = sal*0.225 - 636.13;
+    sal = sal - ir;
+} else if (sal >= 2826.66 && sal <= 3751.05) {
+    ir = sal*0.15 - 354.80;
+    sal = sal - ir;
+} else if (sal >= 1903.99 && sal <= 2826.65) {
+    ir = sal*0.075 - 142.80;
+    sal = sal - ir;
+}
+
+console.log("Salário Líquido: ",sal);
