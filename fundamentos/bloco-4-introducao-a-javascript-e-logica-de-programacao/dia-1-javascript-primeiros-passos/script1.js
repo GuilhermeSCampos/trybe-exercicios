9737f919053079656de7bac9de9e2e15c09dded2
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-06 18:29:04 -0300

    Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
@@ -0,0 +1,8 @@
+const a = 15;
+const b = 12;
+
+console.log("Adição: ",a + b);
+console.log("Subtração: ",a - b);
+console.log("Multiplicação: ",a * b);
+console.log("Divisão: ",a / b);
+console.log("Módulo: ",a % b);
