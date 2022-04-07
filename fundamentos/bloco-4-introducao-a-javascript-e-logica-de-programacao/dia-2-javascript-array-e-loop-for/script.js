5daff5d82cd24f359a51bfdd3473ee6fdffdb390
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-07 17:46:07 -0300

    Utilizando for , descubra qual o menor valor contido no array e imprima-o;

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,10 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let odd = 0;
+let menor = numbers[0];
 
 for (let index = 0; index <numbers.length; index += 1) {
-   if ((numbers[index] % 2) != 0) {
-       odd += 1;
-   }
+    if (menor > numbers[index]) {
+        menor = numbers[index];
+    }
 }
-
-console.log("Quantidade de números ímpares: ",odd);
+console.log("Menor Valor: ",menor);
