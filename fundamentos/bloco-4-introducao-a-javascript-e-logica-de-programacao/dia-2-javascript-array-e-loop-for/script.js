c5890cd4b0c258b4b8e94f346857d1ee691420a1
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-07 19:19:09 -0300

    Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo:

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,12 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers2 = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    for (let index2 = 0; index2 < index; index2 += 1) {
-        if (numbers[index2] < numbers[index]) {
-            let position = numbers[index];
-            numbers[index] = numbers[index2];
-            numbers[index2] = position;
-        }
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index + 1] === undefined) {
+        numbers2.push(numbers[index] * 2);
+    } else {
+        numbers2.push(numbers[index] * numbers[index + 1]);
+    }   
 }
-console.log(numbers);
+console.log(numbers2);
