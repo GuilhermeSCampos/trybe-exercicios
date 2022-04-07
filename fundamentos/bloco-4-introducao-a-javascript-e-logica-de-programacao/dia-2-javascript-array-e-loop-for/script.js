35bd14e1f64f5aac23639285667005bdbddade97
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-07 19:07:12 -0300

    Bônus: Ordene o array numbers em ordem decrescente e imprima seus valores;

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 1; index < numbers.length; index += 1) {
     for (let index2 = 0; index2 < index; index2 += 1) {
-        if (numbers[index2] > numbers[index]) {
+        if (numbers[index2] < numbers[index]) {
             let position = numbers[index];
             numbers[index] = numbers[index2];
             numbers[index2] = position;
