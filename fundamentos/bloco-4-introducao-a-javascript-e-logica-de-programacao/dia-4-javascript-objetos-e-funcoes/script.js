01e645a0cbad8e1c87d48306b33e44d96273ee3d
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-11 19:05:30 -0300

    Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: O livro favorito de Julia Pessoa se chama O Pior Dia de Todos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,17 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-  };
-
-for (let index in info) {
-    console.log(info[index] + " e " + info2[index]);
-}
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + "se chama " + leitor.livrosFavoritos[0].titulo);
