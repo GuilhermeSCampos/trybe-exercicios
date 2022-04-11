3f8337b69b4ade97919de5d7eed587237c101740
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-11 19:51:12 -0300

    Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,13 +1,25 @@
 function smaller(inteiros) {
-    let a = "";
-    for (let index of inteiros) {
-        if (a.length < index.length) {
+    let temp = 0;
+    let rep = 0;
+    let a;
+    for(let index of inteiros) {
+        for(let index2 of inteiros) {
+            if (index === index2) {
+                rep += 1;
+            }
+        }
+        
+        if (rep > temp) {
             a = index;
         }
+        temp = rep;
+        rep = 0;
     }
     return a;
 }
-let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+let array = [2, 3, 2, 5, 8, 2, 3];
+
 console.log(smaller(array));
 
 
