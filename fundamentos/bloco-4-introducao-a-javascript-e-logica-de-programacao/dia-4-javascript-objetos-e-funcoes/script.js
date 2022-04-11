b5c9007e30adf52bea27b8457e56a68ae8ab2175
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-11 19:20:46 -0300

    Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,19 +1,21 @@
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-      {
-        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-        autor: 'JK Rowling',
-        editora: 'Rocco',
-      }
-    ],
-  };
+function verificarPalindromo(palavra) {
+    let cont = palavra.length;
+    for (let index of palavra) {
+        if(index === palavra[cont - 1]) {
+        return true;
+        cont -= 1
+        }
+        else {
+            return false;
+            break;
+        }
+    }
+    
+}
 
-  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
+console.log(verificarPalindromo('desenvolvimento'));
+
+
+
+
+
