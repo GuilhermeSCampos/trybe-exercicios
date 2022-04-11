325a56ec61e7ada04337357adb67d947a3b257fb
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-11 19:33:48 -0300

    Crie uma função que receba um array de inteiros e retorne o índice do menor valor.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,16 +1,16 @@
 function smaller(inteiros) {
-    let temp = 0;
+    let temp = inteiros[0];
     let a;
     for (let index in inteiros) {
-        if (temp < inteiros[index]) {
+        if (temp > inteiros[index]) {
             temp = inteiros[index];
             a = [index];
         }
 
     }
-    return a;
+    return a
 }
-let array = [2, 3, 6, 7, 10, 1];
+let array = [2, 4, 6, 7, 10, 0, -3];
 
 console.log(smaller(array));
 
