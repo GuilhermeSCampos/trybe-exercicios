b38cb585499ba861e57f72748dc0597ee996dd82
Guilherme Campos <guilhermeregular@gmail.com>
2022-04-18 21:30:10 -0300

    exercicio 6

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -95,3 +95,23 @@ botaoSexta.addEventListener("click", function () {
         console.log(buttonClickFriday);
     }
 })
+// exercicio 6
+function mouseOver() {
+    let days = document.querySelector("#days");
+    
+    days.addEventListener("mouseover", function (event) {
+        event.target.style.fontSize = "30px";
+        event.target.style.fontWeight = "600";
+    })
+}
+function mouseOut() {
+    let days = document.querySelector("#days");
+    
+    days.addEventListener("mouseout", function (event) {
+        event.target.style.fontSize = "20px";
+        event.target.style.fontWeight = "200";
+    })
+}
+mouseOver();
+mouseOut();
+
