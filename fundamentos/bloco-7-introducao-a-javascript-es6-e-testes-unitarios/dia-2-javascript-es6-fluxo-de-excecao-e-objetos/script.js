c980fd2e3e2c49d45d0cd0e866ddd8518d489cee
Guilherme Campos <guilhermeregular@gmail.com>
2022-05-09 19:56:32 -0300

    exercicio 4

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -0,0 +1,57 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+    const deliveryPerson = order.order.delivery.deliveryPerson;
+    const name = order.name;
+    const phone = order.phoneNumber;
+    const street = order.address.street;
+    const number = order.address.number;
+    const ap = order.address.apartment;
+    
+    console.log(`Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phone}, R.${street}, Nº: ${number}, AP: ${ap}`);
+  }
+
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
