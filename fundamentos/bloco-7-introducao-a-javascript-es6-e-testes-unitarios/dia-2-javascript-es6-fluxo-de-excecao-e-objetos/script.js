947335993397f731568e8444324f7b7993156401
Guilherme Campos <guilhermeregular@gmail.com>
2022-05-10 13:24:47 -0300

    exercicio 6

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,62 +1,26 @@
-const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
-      },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
-    },
-    payment: {
-      total: 60,
-    },
-  };
-  
-  const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
-    const deliveryPerson = order.order.delivery.deliveryPerson;
-    const name = order.name;
-    const phone = order.phoneNumber;
-    const street = order.address.street;
-    const number = order.address.number;
-    const ap = order.address.apartment;
-    
-    console.log(`Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phone}, R.${street}, Nº: ${number}, AP: ${ap}`);
-  }
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
 
-  
-  customerInfo(order);
-  
-  const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-    const newBuyer = order.name = `Luiz Silva`;
-    const sabor1 = Object.keys(order.order.pizza)[0];
-    const sabor2 = Object.keys(order.order.pizza)[1];
-    const bebida = order.order.drinks.coke.type;
-    const preco = `50`;
-    console.log(`Olá ${newBuyer}, o total do seu pedido de ${sabor1}, ${sabor2} e ${bebida} é ${preco}.`);
-  }
-  
-  orderModifier(order);
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const lessonAssing = (object, key, value) => {
+  const turno = key;
+  object[turno] = value;
+}
+
+lessonAssing(lesson2, 'turno', 'noite');
