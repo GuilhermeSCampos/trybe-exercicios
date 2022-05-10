f5f0ef03c66b16e9d7c899d89c14729bf4a4a174
Guilherme Campos <guilhermeregular@gmail.com>
2022-05-10 14:04:25 -0300

    resto dos exercicios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -24,3 +24,46 @@ const lessonAssing = (object, key, value) => {
 }
 
 lessonAssing(lesson2, 'turno', 'noite');
+
+const listKeys = (object) => {
+  console.log(Object.keys(object));
+}
+
+const listObject = (object) => {
+  console.log(Object.entries(object));
+}
+
+const listValues = (object) => {
+  console.log(Object.values(object));
+}
+
+const allLessons = {};
+Object.assign(allLessons, {lesson1, lesson2, lesson3});
+
+const stundents = (object) => {
+  let number = 0;
+  let keys = Object.keys(object);
+  for (let i = 0; i < keys.length; i += 1) {
+    let key = keys[i];
+    number += object[key].numeroEstudantes;
+  }
+  return number;
+}
+
+const getValueByNumber = (obj, keyposition) => {
+  let keys = Object.keys(obj);
+  return obj[keys[keyposition]];
+}
+
+console.log(getValueByNumber(lesson1, 0));
+
+const verifyPair = (obj, key, value) => {
+  if (obj[key] === value) {
+    return true;
+  } else {
+    return false
+  }
+}
+
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+
