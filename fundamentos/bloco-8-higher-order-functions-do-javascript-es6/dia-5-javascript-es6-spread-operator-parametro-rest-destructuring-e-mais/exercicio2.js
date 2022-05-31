d04accc2065f98b24044cbf812a06fc64ac28776
Guilherme Campos <guilhermeregular@gmail.com>
2022-05-31 18:30:11 -0300

    não sei

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio2.js
@@ -0,0 +1,7 @@
+// escreva sum abaixo
+
+const sum = (...numbers) => {
+    console.log(numbers.reduce((acc, curr) => acc + curr));
+}
+
+sum(1,2,3,4);
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio8.js
@@ -0,0 +1,8 @@
+// escreva greet abaixo
+const greet = (name, greets = 'Hi') => {
+    return `${greets} ${name}`
+}
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
