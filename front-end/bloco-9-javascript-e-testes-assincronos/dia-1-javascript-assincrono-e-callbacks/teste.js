58447f46ee648b15f99b6fd8bc5dbca7cb576cfb
Guilherme Campos <guilhermeregular@gmail.com>
2022-06-01 19:28:37 -0300

    aaa

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/teste.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/teste.js
@@ -1,25 +1,47 @@
-const messageDelay = () => Math.floor(Math.random() * 5000);
-
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
-};
-
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-
-const temperatureInFahrenheit = (temperature) =>
-  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-
-const greet = (temperature) =>
-  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
-
-// definição da função sendMarsTemperature...
-const sendMarsTemperature = (callback) => {
-    const temperature = getMarsTemperature();
-    setTimeout(() => console.log(temperature), messageDelay());
-    callback(temperature);
-}
-
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
-
+const pokemons = [
+    {
+      name: 'Bulbasaur',
+      type: 'Grass',
+      ability: 'Razor Leaf',
+    },
+    {
+      name: 'Charmander',
+      type: 'Fire',
+      ability: 'Ember',
+    },
+    {
+      name: 'Squirtle',
+      type: 'Water',
+      ability: 'Water Gun',
+    },
+  ];
+  
+  function getPokemonDetails(filter, callback) {
+    setTimeout(() => {
+      if (pokemons.find(filter) === undefined) {
+        return callback(new Error('Não temos esse pokémon para você :('), null);
+      }
+      const pokemon = pokemons.find(filter);
+  
+      const { name, type, ability } = pokemon;
+  
+      const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+  
+      callback(null, messageFromProfOak);
+    }, 2000);
+  }
+  
+  getPokemonDetails(
+      (pokemon) => pokemon.name === 'Squi',
+      (error, message) => {
+          if (error) {
+              console.log(error);
+          } else {
+              console.log(message);
+          }
+      }
+  );
+  
+  module.exports = {
+    getPokemonDetails,
+  };
