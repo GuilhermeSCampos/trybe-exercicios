b231932b46eea681e2ccab36a6cea84ac7edabb6
Guilherme Campos <guilhermeregular@gmail.com>
2022-06-01 21:59:49 -0300

    exercicio 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -1,17 +1,18 @@
 // apiScript.js
-const API_URL = 'https://icanhazdadjoke.com/';
+const API_URL = `https://api.coincap.io/v2/assets`;
+const list = document.getElementById("list");
 
-const fetchJoke = async() => {
-  const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  };
-
-  const joke = await fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => data.joke);   
-    const jokeContainer = document.getElementById("jokeContainer");
-    jokeContainer.innerText = joke;
+const fetchAppend = async() => {
+    const data = await fetch(API_URL);
+    const json = await data.json();
+    const moedas = json.data;
+    
+    for (let i = 0; i < 10; i += 1) {
+        const newLi = document.createElement('li');
+        console.log(moedas[i]);
+        newLi.innerText = `${moedas[i].name} (${moedas[i].symbol}): ${Number(moedas[i].priceUsd).toFixed(2)}`;
+        list.appendChild(newLi);
+    }
 };
 
-window.onload = () => fetchJoke();
+window.onload = () => fetchAppend();
