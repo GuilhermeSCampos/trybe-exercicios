7dc3c3c036aa73c56ac9077ea3f80237196ba429
Guilherme Campos <guilhermeregular@gmail.com>
2022-06-23 19:12:40 -0300

    oxe

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -2,10 +2,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbers2 = [];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index + 1] === undefined) {
-        numbers2.push(numbers[index] * 2);
-    } else {
+    if (numbers[index + 1] !== undefined) {
         numbers2.push(numbers[index] * numbers[index + 1]);
+    } else {
+        numbers2.push(numbers[index] * 2);
     }   
 }
 console.log(numbers2);
